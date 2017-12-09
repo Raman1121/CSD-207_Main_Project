1f15d3139ff1a8bb6276a5c5ff48fb10723ad6a6
Shashvat Kedia <sk261@snu.edu.in>
2017-12-09 15:46:42 +0530

    Credentials Added :D

--- Main_Project/src/main/java/Mongo.java
+++ Main_Project/src/main/java/Mongo.java
@@ -5,7 +5,7 @@ import com.mongodb.*;
 public class Mongo {
 
     public static void addDocument(String selected){
-        MongoClientURI uri = new MongoClientURI("mongodb://<Id>:<Password>@ds127536.mlab.com:27536/csd-207_main_project");
+        MongoClientURI uri = new MongoClientURI("mongodb://user1:userDataPassword@ds127536.mlab.com:27536/csd-207_main_project");
         MongoClient client = new MongoClient(uri);
         DB database = client.getDB("csd-207_main_project");
         DBCollection collection = database.getCollection("UserTunes");
